3c7b374e276602c916f45c1f51e32ee796ff3922
HelloWorllds <dark____angel@mail.ru>
2018-07-15 14:42:47 +0300

    play with animation

--- src/components/SliderItem.js
+++ src/components/SliderItem.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
 
 export default class SliderItem extends Component {
 	render() {
 		return (
 			<div className='portfolio__item' style={{backgroundImage: `url(${this.props.img})`}}>
-				<div className='portfolio__item-container'>
-					<a href={this.props.url} target='_blank' className='portfolio__item-link' title={'link to - ' + this.props.name}>{this.props.name}</a>
-				</div>
+				<CSSTransitionGroup 
+					transitionName='showlink'
+					transitionEnterTimeout={500} 
+					transitionLeave={false}
+				>
+					<div key={this.props.name} className='portfolio__item-container'>
+						<a href={this.props.url} target='_blank' className='portfolio__item-link' title={'link to - ' + this.props.name}>{this.props.name}</a>
+					</div>
+				</CSSTransitionGroup>
 			</div>
 		);
 	}
