5b7121441573b09cf6f7a6c2d49c16f17548d47f
HelloWorllds <dark____angel@mail.ru>
2018-07-15 16:05:30 +0300

    add 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,11 +5,12 @@ export default class NotFound extends Component {
 	render() {
 		return (
 			<div className='error-page'>
-				404
-				<br />
-				Page not found
-				<br />
-				<Link to='/'>Home</Link>
+				<Link to='/' className='main-logo'><img src='/images/logo.png' alt='logo'/></Link>
+				<div className='error-page__text'>Psst, hey you! <br /> Are you looking something?</div>
+				<div className='error-page__btn-wrapper'>
+					<Link to='/' className='error-page__link'>Go home</Link>
+				</div>
+				<footer className='footer'>© 2018 Vladimir Lysiuk</footer>
 			</div>
 		)
 	}
