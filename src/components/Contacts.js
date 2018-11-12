d500466f53c21364cf1c08bdbed177d58522d6e8
HelloWorllds <dark____angel@mail.ru>
2018-11-12 23:58:10 +0200

    fix contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -27,19 +27,19 @@ export default class Contacts extends Component {
 				<div className='contacts__container'>
 					<div className='contacts__header'>Contacts</div>
 					<div className='contacts__links'>
-						<a href='/' target='_blank' className='contacts__link contacts__link_linkedin'>
+						<a href='https://www.linkedin.com/in/v-lysiuk/' target='_blank' className='contacts__link contacts__link_linkedin'>
 							<div className='contacts__tooltip'>LinkedIn</div>
 						</a>
-						<a href='/' target='_blank' className='contacts__link contacts__link_github'>
+						<a href='https://github.com/HelloWorllds' target='_blank' className='contacts__link contacts__link_github'>
 							<div className='contacts__tooltip'>GitHub</div>
 						</a>
-						<a href='/' target='_blank' className='contacts__link contacts__link_codepen'>
+						<a href='https://codepen.io/HelloWorllds/' target='_blank' className='contacts__link contacts__link_codepen'>
 							<div className='contacts__tooltip'>CodePen</div>
 						</a>
-						<a href='/' target='_blank' className='contacts__link contacts__link_e-mail'>
+						<a href='mailto:vladimir.lysuk@gmail.com' target='_blank' className='contacts__link contacts__link_e-mail'>
 							<div className='contacts__tooltip'>E-mail</div>
 						</a>
-						<a href='/' target='_blank' className='contacts__link contacts__link_resume'>
+						<a href='https://drive.google.com/open?id=1F6YrEqv89Klaevne4gzquOGPytOJMnxw' target='_blank' className='contacts__link contacts__link_resume'>
 							<div className='contacts__tooltip'>Resume</div>
 						</a>
 					</div>
